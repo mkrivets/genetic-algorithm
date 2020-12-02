661712e1eeb5f58744b28bfd72258236415ecf18
mkrivets <mkrivets1@gmail.com>
2020-12-02 07:06:54 +0200

    added a comparison table

--- comparing.py
+++ comparing.py
@@ -9,6 +9,7 @@ print('\n')
 print("Where the first component is the minimum of the function, the other 2 are "
       "coordinates of a point at which this minimum is attained")
 print('\n')
+print("Comparison table:\n")
 print("Random Search\t\t\t\t", "Simulated Annealing\t\t", "Genetic Algorithm")
 for i in range(0,10):
       print(f'{randomSearch()[0]:.{12}f}', '\t\t\t', f'{annealing()[0]:.{12}f}', '\t\t\t', f'{genetic()[0]:.{12}f}')
